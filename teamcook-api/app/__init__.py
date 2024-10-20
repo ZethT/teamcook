536e26874b6e794cc2ae459caae3c16bb92b484a
Grid <101964953+ZethT@users.noreply.github.com>
2024-10-19 19:00:03 -0700

    __init__.py + recipe frontend update
    __init__.py
    imported CORS for
    request handling
    RECIPEBOOK.js updated

--- teamcook-api/app/__init__.py
+++ teamcook-api/app/__init__.py
@@ -4,12 +4,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config  # Ensure this import statement exists
+from flask_cors import CORS
+
 
 db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(config_class)
 
     # Initialize extensions
